9972bdcb3c0fa2c2b2da7973c6bec6610aa2fed9
Alexandre Marteau <alexandre.marteau63@gmail.com>
2025-10-18 20:41:08 +0200

    Génération automatique de la clé key1

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,24 @@
 """Modèles de l’application."""
 
+import secrets
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def generate_user_key1(sender, instance, created, **kwargs):
+    """Génère automatiquement une clé `key1` pour l’utilisateur. """
+    if not created:
+        return
+    if hasattr(instance, "key1") and not getattr(instance, "key1"):
+        instance.key1 = secrets.token_urlsafe(32)
+        try:
+            instance.save(update_fields=["key1"])
+        except Exception:
+            instance.save()
+
 
